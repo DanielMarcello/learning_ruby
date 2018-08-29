4085ccf1b75b00558c72234be5393e88d4c12f39
Daniel_Marcello <daniel.salinas.tellez@gmail.com>
2018-08-28 21:06:48 -0700

    Hello World

---
+++ HelloWorld.rb
@@ -0,0 +1,14 @@
+class HelloWorld
+    def initialize()
+    end
+
+    def say_hello()
+        puts "Hello World"
+    end
+end
+
+helloObject = HelloWorld.new()
+helloObject.say_hello()
+
+# User input
+gets()
