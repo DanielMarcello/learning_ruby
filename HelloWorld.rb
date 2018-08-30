c0c405f5b6e8d544f43b96d85df2de79a9718629
Daniel_Marcello <daniel.salinas.tellez@gmail.com>
2018-08-29 18:53:25 -0700

    String management

--- HelloWorld.rb
+++ HelloWorld.rb
@@ -20,6 +20,28 @@ class HelloWorld
 
         puts $global_variable
         puts @global_variable
+
+        addition = 2 + 3
+        substraction = 3 - 1
+        multiplication = 2 * 5
+        division = 7/2
+        power = 2**3
+        puts addition
+        puts substraction
+        puts multiplication
+        puts division
+        puts power
+
+        res = 2 + 3
+        puts "the result of the addition of 2 + 3 is: #{res}"
+        sentence = "Hello"
+        sentence << " world"
+        sentence.concat(33)
+        sentence << " " << "!" * 4
+        puts sentence
+        word1 = "Hello"
+        word2 = "Hello"
+        puts word1 <=> word2 #1 when left > right, -1 when left < right, 0 when left == right
     end
 end
 
