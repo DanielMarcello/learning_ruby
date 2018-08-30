d1921233ed7d87db01f5f174d5a7bca3bda2da27
Daniel_Marcello <daniel.salinas.tellez@gmail.com>
2018-08-29 21:15:20 -0700

    Nested def
    Works ONLY when the parent method is called

--- Main.rb
+++ Main.rb
@@ -3,3 +3,22 @@ objDog = Dog.new()
 objDog.bark
 objDog2 = Dog.new("Chamaco")
 objDog2.bark
+
+class Xclass
+    def initialize()
+    end
+    def xDef
+        puts "X"
+        def yDef
+            puts "y"
+        end
+        def zDef
+            puts "z"
+        end
+    end
+end
+
+obj = Xclass.new()
+obj.xDef
+obj.yDef #Works ONLY when the parent method is called, in this case "xDef"
+obj.zDef #Works ONLY when the parent method is called, in this case "xDef"
