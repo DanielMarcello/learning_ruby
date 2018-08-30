e2db82162548857e58074fc5c87eb78e57d2fc75
Daniel_Marcello <daniel.salinas.tellez@gmail.com>
2018-08-29 21:08:57 -0700

    Classes, how to call files and making constructors

---
+++ Dog.rb
@@ -0,0 +1,8 @@
+class Dog
+    def initialize(name = 'Lobito')
+        @name = name
+    end
+    def bark
+        puts "I'm #{@name}, and I say WOOF!!!"
+    end
+end
---
+++ Main.rb
@@ -0,0 +1,5 @@
+require_relative 'Dog.rb'
+objDog = Dog.new()
+objDog.bark
+objDog2 = Dog.new("Chamaco")
+objDog2.bark
